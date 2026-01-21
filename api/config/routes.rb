dd2a82b46f77c98d405e964b38b5d3f02712aa23
tokio <surfin8000@gmail.com>
2026-01-21 09:22:39 +0900

    Devise,devise+JWT追加

--- api/config/routes.rb
+++ api/config/routes.rb
@@ -1,9 +1,21 @@
+require 'devise'
+
 Rails.application.routes.draw do
+  # devise_for :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
   # Can be used by load balancers and uptime monitors to verify that the app is live.
   get "up" => "rails/health#show", as: :rails_health_check
+
+  devise_for :users,
+    defaults: { format: :json },
+    path: "",
+    path_names: {
+      sign_in: "api/login",
+      sign_out: "api/logout",
+      registration: "api/signup"
+    }
 
   # Defines the root path route ("/")
   # root "posts#index"
@@ -15,4 +27,5 @@ Rails.application.routes.draw do
     # ユーザ関連
     resources :users
   end
+
 end
