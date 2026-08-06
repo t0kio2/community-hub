7168fb1b437b718a79ca3d06970b539b18ac0ba6
tokio <surfin8000@gmail.com>
2026-08-06 23:29:57 +0900

    Hotwire, propshaft導入

--- backend/app/controllers/admin/home_controller.rb
+++ backend/app/controllers/admin/home_controller.rb
@@ -1,4 +1,9 @@
 class Admin::HomeController < Admin::BaseController
   def index
+    @articles = [
+      { id: 1, title: "Rails 8の新機能について", content: "KamalやSolid Adapterが標準搭載されました。" },
+      { id: 2, title: "Hotwireの使い方入門", content: "Turbo Framesを使うと、非同期更新が超簡単になります！" }
+    ]
   end
+  
 end
