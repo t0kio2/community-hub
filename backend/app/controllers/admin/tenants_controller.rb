f1610cbc8dfea912f99e33bcae7bd90310628e2e
tokio <surfin8000@gmail.com>
2026-08-10 16:54:56 +0900

    テナント管理,index追加

---
+++ backend/app/controllers/admin/tenants_controller.rb
@@ -0,0 +1,47 @@
+class Admin::TenantsController < Admin::BaseController
+  before_action :set_tenant, only: %i[show edit update destroy]
+  before_action :authorize_tenant!
+	# TODO: docs/FAQ/authorize_tenant_account?.txt を元に、どうするべきか考える。
+
+	def index
+		@tenants = Tenant.order(id: :desc)
+		puts "###############{@tenants.inspect}"
+	end
+
+	def new
+	end
+
+	def show
+	end
+
+	def edit
+	end
+
+	def update
+	end
+
+	def destroy
+	end
+
+	private
+
+	def authorize_tenant!
+		record = @tenant || Tenant
+		query = {
+			"index" => :index?,
+      "show" => :show?,
+      "new" => :create?,
+      "create" => :create?,
+      "edit" => :update?,
+      "update" => :update?,
+      "destroy" => :destroy?
+		}.fetch(action_name)
+
+		authorize record, query, with: Admin::TenantPolicy
+	end
+
+	def set_tenant
+    @tenant = Tenant.find(params[:id])
+  end
+
+end
