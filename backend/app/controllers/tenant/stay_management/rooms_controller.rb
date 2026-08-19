ebfc243406febe204aaeb7655d7cf68e094f79ef
tokio <surfin8000@gmail.com>
2026-08-19 19:05:07 +0900

    客室管理追加

--- backend/app/controllers/tenant/stay_management/rooms_controller.rb
+++ backend/app/controllers/tenant/stay_management/rooms_controller.rb
@@ -1,6 +1,40 @@
 class Tenant::StayManagement::RoomsController < Tenant::StayManagement::BaseController
 
 	def index
-		@rooms = 
+		set_rooms
 	end
+
+	def new
+		@room = @stay_listing.stay_rooms.new
+	end
+
+	def create
+		@room = @stay_listing.stay_rooms.new(room_params)
+
+		if @room.save
+			redirect_to tenant_stay_rooms_path(@listing), notice: "客室を登録しました"
+		else
+			render :new, status: :unprocessable_entity
+		end
+	end
+
+	def edit
+	end
+
+	def update
+	end
+
+	def destroy
+	end
+
+	private
+
+	def set_rooms
+		@rooms = @stay_listing.stay_rooms
+	end
+
+	def room_params
+		params.require(:stay_room).permit(:name, :stay_room_type_id, :active, :notes)
+	end
+
 end
